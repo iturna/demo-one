204d7d57d827f39d08cb8923a88e9f537a037b3e
iturna <ismaturna@hotmail.com>
2024-09-24 10:24:12 +0300

    updateOurString added

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -5,4 +5,8 @@ pragma solidity 0.8.26;
 contract MyContract {
 
     string public ourString = "World!123123";
+
+    function updateOurString(string memory _updateString) public {
+        ourString = _updateString;
+    }
 }
